6e4a02eca85c917d903a797a91de574468788699
luthfimawarid <lutfhiemawarid@gmail.com>
2023-11-09 13:40:35 +0700

    nina

---
+++ resources/views/deskripsimenu/header.blade.php
@@ -0,0 +1,6 @@
+<div class="alert alert-primary mb-0" role="alert">
+    <div class="container">
+        <h1>Temukan kelembutan dan kelezatan autentik dari kue pancong</h1>
+        <h5>Aroma dari kue pancong dan kelembutannya mengajak jiwa untuk memulai petualangan rasa dari setiap suapan</h5>
+    </div>
+</div>
