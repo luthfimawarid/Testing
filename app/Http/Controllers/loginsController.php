fbcec970fec419057b967f89086f52c40ea84abd
andini92 <andiniaprilian36@gmail.com>
2023-12-04 11:27:09 +0700

    History

--- app/Http/Controllers/loginsController.php
+++ app/Http/Controllers/loginsController.php
@@ -8,6 +8,22 @@ class loginsController extends Controller
 {
     public function login()
     {
-        return view('login');
+        return view('login.home');
+    }
+
+    public function loginpost(Request $request)
+    {
+        $login = new Login();
+
+        // $registrasi->id = $request->id;
+        $login->username = $request->username;
+        $login->password = $request->password;
+        $login->alamat = $request->alamat;
+        $login->email = $request->email;
+        
+
+        $login->save();
+
+       
     }
 }
